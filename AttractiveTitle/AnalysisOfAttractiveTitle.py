912614b0e7e07c2cf07583c3e2cc6472cbec20b0
Wonil <red112@naver.com>
2023-09-20 00:05:44 +0900

    제목의 명사별 조회수 분석

--- AttractiveTitle/AnalysisOfAttractiveTitle.py
+++ AttractiveTitle/AnalysisOfAttractiveTitle.py
@@ -10,11 +10,18 @@ kkma = Kkma()
 
 dc_dict={}
 
-#list_file = open('./AttractiveTitle/DCInside_gogall_list_with_reply.text')
-list_file = open('./AttractiveTitle/1.text', encoding='utf-8')
+list_file = open('./AttractiveTitle/DCInside_gogall_list_with_reply.text', encoding='utf-8')
+#list_file = open('./AttractiveTitle/1.text', encoding='utf-8')
 
-for line in list_file.readlines():
+cnt = 0
+lines = list_file.readlines()
+line_cnt = len(lines)
+for line in lines:
     line = line.strip()
+
+    cnt += 1
+    if cnt % 100 == 0:
+        print(f"Progress - {cnt} / {line_cnt}")
 
     # 1842020|정신차리자 개이야 |4|고갤러(118.235)|2023-08-31 20:34:14|32|0
     terms = line.split('|')
@@ -27,18 +34,21 @@ for line in list_file.readlines():
         continue
 
     # 제목의 형태소 분리
-    morphs = kkma.morphs(title)
+    #morphs = kkma.morphs(title)
+    #morphs = kkma.pos(title)
+    nouns = kkma.nouns(title)
 
-    for morph in morphs:
+    for morph in nouns:
         if morph in dc_dict:
             dc_dict[morph] += views
         else:
             dc_dict[morph] = views
 list_file.close()
 
-dc_dict = sorted(dc_dict.items(), key=itemgetter(1))
+dc_dict = sorted(dc_dict.items(), key=itemgetter(1), reverse=True)
 
-with open('saved_dictionary.pkl', 'wb') as f:
+with open('saved_nouns_dictionary.pkl', 'wb') as f:
     pickle.dump(dc_dict, f)
 
+dc_dict = dc_dict[:100]
 print(dc_dict)
